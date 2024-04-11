a69eb5ca63c89d0583ed0d7c061a0b5ab2ea7a49
Andrew Rizk <97995173+AndrewidRizk@users.noreply.github.com>
2024-04-11 16:59:12 -0400

    fixing up the maze making two openings

--- mazeGenerator.js
+++ mazeGenerator.js
@@ -406,6 +406,20 @@ function generateMaze() {
 function SolveMazeApi(){
   console.log("Solve Maze API button clicked");
   const mazeData = newMaze.getMaze_creator();
+  let firstElement = mazeData[0][0]; // Get the first element of the first row
+  // Replace the first character with "0"
+  firstElement = '0' + firstElement.substring(1);
+  // Assign the modified string back to the first element of the first row
+  mazeData[0][0] = firstElement;
+  let lastRowIndex = mazeData.length - 1; // Index of the last row
+  let lastColIndex = mazeData[lastRowIndex].length - 1; // Index of the last element in the last row
+  let lastElement = mazeData[lastRowIndex][lastColIndex]; // Get the last element of the last row
+
+  // Replace the last character with "0"
+  lastElement = lastElement.substring(0, lastElement.length - 1) + '0';
+  
+  // Assign the modified string back to the last element of the last row
+  mazeData[lastRowIndex][lastColIndex] = lastElement;
 
   fetch('http://localhost:8080/solve', {
       method: 'POST',
